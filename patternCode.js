fabb501a349f9880ea9a0704c602cfbed99c2122
Sanket Mane <sanketmane2255@gmail.com>
2026-03-15 12:37:31 +0530

    Updated code

--- patternCode.js
+++ patternCode.js
@@ -1,6 +1,20 @@
-let Number = 14641;
 
-for (let i = 0; i < 5; i++) {
-    console.log(Number);
-    Number = Number / 11;
+function printPattern(n) {
+
+    for (let i = 0; i < n; i++) {
+        let row = "";
+        for (let s = 0; s < i; s++) {
+            row += " ";
+        }
+        let val = 1;
+
+        for (let j = 0; j < n - i; j++) {
+
+            row += val + " ";
+            val = val * (n - i - 1 - j) / (j + 1);
+        }
+        console.log(row);
+    }
 }
+
+printPattern(5);
